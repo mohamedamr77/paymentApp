c4e7472eea9688120852ab53db573a60a79acc2d
mohamedamr77 <moamr947@gmail.com>
2024-10-02 18:10:09 +0300

    initial commit

---
+++ lib/core/navigation/routes.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+
+
+class AppRouter {
+  static Map<String, Widget Function(dynamic)> routes = {
+    // SplashScreen.id: (context) => const SplashScreen(),
+
+  };
+}
