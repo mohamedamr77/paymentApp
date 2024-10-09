4be3e1920c8863658e4a5bcb1149660c32b9adbf
mohamedamr77 <moamr947@gmail.com>
2024-10-09 15:25:23 +0300

    make deshed line

--- lib/features/my_cart/presentation/view/widgets/thank_you_body.dart
+++ lib/features/my_cart/presentation/view/widgets/thank_you_body.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:paymentapp/core/constant/color_app.dart';
+import 'package:paymentapp/core/constant/text_app.dart';
 import 'package:paymentapp/core/extentions/screen_size.dart';
 import 'package:paymentapp/core/utils/shared_widget/custom_abb_bar.dart';
+import 'package:paymentapp/core/utils/shared_widget/custom_text_with_price.dart';
+import 'package:paymentapp/core/utils/shared_widget/global_text.dart';
+
+import '../../../../../core/utils/shared_widget/deshed_line.dart';
 
 class ThankYouBody extends StatelessWidget {
   const ThankYouBody({super.key});
@@ -30,6 +35,54 @@ class ThankYouBody extends StatelessWidget {
                   borderRadius: BorderRadius.circular(0.05.w),
                   color: const Color(0xffEDEDED),
                 ),
+                child: Column(
+                  children: [
+                    0.07.ph,
+                    GText(
+                        content:  AppText.kThankYou,
+                        fontSize: 0.07.w,
+                          fontWeight: FontWeight.w700,
+                    ),
+                    0.01.ph,
+                    GText(
+                        content:  AppText.kYourTransactionWasSuccessful,
+                        fontSize: 0.05.w,
+                          textAlign: TextAlign.center,
+                          fontWeight: FontWeight.w400,
+                    ),
+                    0.04.ph,
+                    const CustomTextWithPrice(
+                        text: AppText.kDate,
+                        price: "01/24/2023"
+                    ),
+                    0.01.ph,
+                    const CustomTextWithPrice(
+                        text: AppText.kTime,
+                        price: "10:15 AM"
+                    ),
+                    0.01.ph,
+                    const CustomTextWithPrice(
+                        text: AppText.kTo,
+                        price: "Sam Louis"
+                    ),
+                    0.02.ph,
+                    Divider(
+                      indent: 0.04.w,
+                      endIndent: 0.04.w,
+                    ),
+                    0.02.ph,
+                    CustomTextWithPrice(
+                      text: AppText.kTotal,
+                      price: "\$50.97",
+                      fontWeightPrice: FontWeight.bold,
+                      fontWeightText: FontWeight.bold,
+                      fontSizeText: 0.05.w,
+                      fontSizePrice: 0.05.w,
+                    ),
+                    0.02.ph,
+                    dashedLine(),
+                  ],
+                ),
               ),
               Positioned(
                 left: 0,
@@ -37,7 +90,7 @@ class ThankYouBody extends StatelessWidget {
                 top: -10,
                 child: CircleAvatar(
                   radius: 0.09.w,
-                  backgroundColor: Color(0xffEDEDED),
+                  backgroundColor: const Color(0xffEDEDED),
                   child: CircleAvatar(
                     backgroundColor: AppColor.greenColor,
                     radius: 0.07.w,
@@ -54,6 +107,7 @@ class ThankYouBody extends StatelessWidget {
                   bottom: 0.25.h,
                   right: 0.03.w,
                   child: CircleAvatar(radius: 0.04.w,backgroundColor: AppColor.whiteColor,)),
+
             ]
           ),
         ],
@@ -61,3 +115,4 @@ class ThankYouBody extends StatelessWidget {
     );
   }
 }
+
