1244c7ac55f8dfb1e6908f4b935134618a982558
mohamedamr77 <moamr947@gmail.com>
2024-10-12 15:34:49 +0300

    make api service

--- lib/core/helper/api_service.dart
+++ lib/core/helper/api_service.dart
@@ -1,106 +1,19 @@
 import 'package:dio/dio.dart';
 
-class ApiService {
-  final _baseUrl = "https://www.googleapis.com/books/v1/";
+class ApiService{
+  final Dio dio =Dio();
+  Future<Response> post({required body, required String url , required String token , String? contentType}) async{
+  var response = await dio.post(
+      url,
+     data: body,
+     options: Options(
+       contentType: contentType,
+      headers: {
+        'Authorization': 'Bearer $token',
 
-  late Dio dio;
-
-  ApiService() {
-    dio = Dio(
-      BaseOptions(
-        baseUrl: _baseUrl,
-        headers: {
-          "accept": "*/*",
-        },
-        connectTimeout: const Duration(seconds: 3),
-      ),
+      }
+     ),
     );
-  }
-
-  Future<dynamic> get({
-    required String endpoint,
-  }) async {
-    var response = await dio.get(
-      endpoint,
-    );
-
-    return response.data;
-  }
-
-/*
-  Future<dynamic> postFormData({
-    required String endpoint,
-    required dynamic data,
-  }) async {
-    AppLogger.print("start register");
-
-    FormData formData = FormData.fromMap(data);
-    dio.options.headers.addAll({
-      "Content-Type": "multipart/form-data",
-    });
-
-    var response = await dio.post(endpoint, data: formData);
-
-    return response.data;
-  }
- */
-  Future<dynamic> post({
-    required String endpoint,
-    Map<String, dynamic>? data,
-  }) async {
-    dio.options.headers.addAll({
-      "Content-Type": "application/json",
-    });
-
-    var response = await dio.post(
-      endpoint,
-      data: data,
-    );
-
-    return response.data;
-  }
-
-  Future<dynamic> put({
-    required String endpoint,
-    Map<String, dynamic>? data,
-  }) async {
-    dio.options.headers.addAll({
-      "Content-Type": "application/json",
-    });
-
-    var response = await dio.put(
-      endpoint,
-      data: data,
-    );
-
-    return response.data;
-  }
-
-  Future<dynamic> postFormData({
-    required String endpoint,
-    required dynamic data,
-  }) async {
-    FormData formData = FormData.fromMap(data);
-    dio.options.headers.addAll({
-      "Content-Type": "multipart/form-data",
-    });
-
-    var response = await dio.post(endpoint, data: formData);
-
-    return response.data;
-  }
-
-  Future<dynamic> putFormData({
-    required String endpoint,
-    required dynamic data,
-  }) async {
-    FormData formData = FormData.fromMap(data);
-    dio.options.headers.addAll({
-      "Content-Type": "multipart/form-data",
-    });
-
-    var response = await dio.put(endpoint, data: formData);
-
-    return response.data;
+  return response;
   }
 }
---
+++ lib/core/service/stripe_service/stripe_servcie.dart
@@ -0,0 +1,8 @@
+import 'package:paymentapp/features/my_cart/data/model/payment_intenet_model.dart';
+
+class StripeService{
+  //
+  // PaymentIntentModel createPaymentIntent({required PaymentIntentModel paymentIntentModel}){
+  //
+  // }
+}
