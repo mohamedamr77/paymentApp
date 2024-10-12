039fa9f1c144241410a9caf425e0a5e8b5d9151b
mohamedamr77 <moamr947@gmail.com>
2024-10-12 15:57:54 +0300

    make api key file and hit ignore

--- lib/core/service/stripe_service/stripe_servcie.dart
+++ lib/core/service/stripe_service/stripe_servcie.dart
@@ -1,8 +1,13 @@
-import 'package:paymentapp/features/my_cart/data/model/payment_intenet_model.dart';
-
-class StripeService{
-  //
-  // PaymentIntentModel createPaymentIntent({required PaymentIntentModel paymentIntentModel}){
-  //
-  // }
-}
+// import 'package:paymentapp/core/helper/api_service.dart';
+// import 'package:paymentapp/features/my_cart/data/model/payment_intenet_model.dart';
+//
+// class StripeService{
+//    final ApiService apiService=ApiService();
+//   PaymentIntentModel createPaymentIntent({required PaymentIntentModel paymentIntentModel}){
+//      apiService.post(
+//          body: paymentIntentModel.toJson(),
+//          url: "https://api.stripe.com/v1/payment_intents",
+//          token: "sk_test_51Q89BpJztipo55VD4lbLBpUQRmvB7nvG5jYCrsZSbKpSFl9HJPrOzODwzSfpeW818BN1kXyH2oIQU817Ews8QHQr00iF1C4qGc"
+//      );
+//   }
+// }
