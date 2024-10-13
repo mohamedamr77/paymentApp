963af5edcda7c88c93395d697db5f27a72fda559
mohamedamr77 <moamr947@gmail.com>
2024-10-13 21:21:52 +0300

    make to video 73

--- lib/features/my_cart/data/model/paypal/item_list_model.dart
+++ lib/features/my_cart/data/model/paypal/item_list_model.dart
@@ -1,18 +1,54 @@
-class Item {
+import 'dart:convert';
+
+import 'package:paymentapp/features/my_cart/data/model/paypal/shipping_address_model.dart';
+
+class ItemListModel {
+  List<OrderItemModel> items;
+  ShippingAddress? shippingAddress;
+
+  ItemListModel({
+    required this.items,
+     this.shippingAddress,
+  });
+
+  factory ItemListModel.fromJson(Map<String, dynamic> json) {
+    var itemList = json['items'] as List;
+    List<OrderItemModel> itemObjects = itemList.map((itemJson) => OrderItemModel.fromJson(itemJson)).toList();
+
+    return ItemListModel(
+      items: itemObjects,
+      shippingAddress: ShippingAddress.fromJson(json['shipping_address']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'items': items.map((item) => item.toJson()).toList(),
+      'shipping_address': shippingAddress?.toJson(),
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Order(items: $items, shippingAddress: $shippingAddress)';
+  }
+}
+
+class OrderItemModel {
   String name;
   int quantity;
   String price;
   String currency;
 
-  Item({
+  OrderItemModel({
     required this.name,
     required this.quantity,
     required this.price,
     required this.currency,
   });
 
-  factory Item.fromJson(Map<String, dynamic> json) {
-    return Item(
+  factory OrderItemModel.fromJson(Map<String, dynamic> json) {
+    return OrderItemModel(
       name: json['name'],
       quantity: json['quantity'],
       price: json['price'],
@@ -34,6 +70,7 @@ class Item {
     return 'Item(name: $name, quantity: $quantity, price: $price, currency: $currency)';
   }
 }
+
 
 
 
--- lib/features/my_cart/presentation/view/widgets/payment_method_bottom_sheet.dart
+++ lib/features/my_cart/presentation/view/widgets/payment_method_bottom_sheet.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 import 'package:paymentapp/core/extentions/screen_size.dart';
+import 'package:paymentapp/features/my_cart/data/model/paypal/amount_transactions_model.dart';
+import 'package:paymentapp/features/my_cart/data/model/paypal/item_list_model.dart';
 import 'package:paymentapp/features/my_cart/presentation/view/widgets/payment_method_list_view.dart';
 import 'package:paymentapp/features/my_cart/presentation/view_model/stripe_payment/stripe_payment_cubit.dart';
 import 'package:paymentapp/features/my_cart/presentation/view_model/stripe_payment/stripe_payment_state.dart';
@@ -49,56 +51,41 @@ class PaymentMethodBottomSheetBody extends StatelessWidget {
                         //                 amount: '100',
                         //                 currency: 'USD',
                         //                 customerId: 'cus_R1HkyfT0NebIzz'));
+
+                        var amount =AmountTransactionsModel(
+                          currency: 'USD',
+                          details:  DetailsAmountTransactionsModel(
+                            shipping: "0",
+                            shippingDiscount: 0,
+                            subtotal: "100",
+                          ),
+                          total: "100",
+                        );
+
+                        List<OrderItemModel> orders=[
+                          OrderItemModel(name: "Apple", quantity: 10, price: "4", currency: "USD"),
+                          OrderItemModel(name: "Apple", quantity: 10, price: "4", currency: "USD"),
+                          OrderItemModel(name: "Apple", quantity: 10, price: "4", currency: "USD"),
+                          OrderItemModel(name: "Apple", quantity: 10, price: "4", currency: "USD"),
+                        ];
+
+
+                        var itemList = ItemListModel(items: orders,);
                         Navigator.of(context).push(MaterialPageRoute(
                           builder: (BuildContext context) => PaypalCheckoutView(
                             sandboxMode: true,
                             clientId: "YOUR CLIENT ID",
                             secretKey: "YOUR SECRET KEY",
-                            transactions: const [
+                            transactions:  [
                               {
                                 // description order from price
-                                "amount": {
-                                  "total": '100',
-                                  "currency": "USD",
-                                  "details": {
-                                    "subtotal": '100',
-                                    "shipping": '0',
-                                    "shipping_discount": 0
-                                  }
-                                },
+                                "amount": amount.toJson(),
                                 "description": "The payment transaction description.",
                                 // "payment_options": {
                                 //   "allowed_payment_method":
                                 //       "INSTANT_FUNDING_SOURCE"
                                 // },
-                                "item_list": {
-                                  "items": [
-                                    {
-                                      "name": "Apple",
-                                      "quantity": 4,
-                                      "price": '10',
-                                      "currency": "USD"
-                                    },
-                                    {
-                                      "name": "Pineapple",
-                                      "quantity": 5,
-                                      "price": '12',
-                                      "currency": "USD"
-                                    }
-                                  ],
-
-                                  // Optional
-                                  //   "shipping_address": {
-                                  //     "recipient_name": "Tharwat samy",
-                                  //     "line1": "tharwat",
-                                  //     "line2": "",
-                                  //     "city": "tharwat",
-                                  //     "country_code": "EG",
-                                  //     "postal_code": "25025",
-                                  //     "phone": "+00000000",
-                                  //     "state": "ALex"
-                                  //  },
-                                }
+                                "item_list": itemList.toJson(),
                               }
                             ],
                             note: "Contact us for any questions on your order.",
